f8baed5b5078e98b96169f9b2dd1173383ebe914
Rebeca Pacheco <rbkgarner@gmail.com>
2026-06-11 16:38:40 -0300

    chore: update repository URLs in package.json and site-config.ts to reflect new ownership

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -2,9 +2,9 @@ export const siteConfig = {
   name: "OpenToolbox",
   tagline: "Your open tools directory",
   githubRepoUrl:
-    process.env.NEXT_PUBLIC_GITHUB_REPO_URL ?? "https://github.com/open-toolbox/open-toolbox",
+    process.env.NEXT_PUBLIC_GITHUB_REPO_URL ?? "https://github.com/rebecagrn/open-toolbox",
   githubIssuesUrl:
     process.env.NEXT_PUBLIC_GITHUB_REPO_URL != null
       ? `${process.env.NEXT_PUBLIC_GITHUB_REPO_URL}/issues`
-      : "https://github.com/open-toolbox/open-toolbox/issues",
+      : "https://github.com/rebecagrn/open-toolbox/issues",
 }
